45af6e5120af7e2042e2a6e6eb3df711610c4509
Matt Thees <mthees7@gmail.com>
2013-10-22 07:57:18 -0400

    mountain-lab

---
+++ w04/d02/matthew/mountains_lab/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MountainsLab::Application.config.session_store :cookie_store, key: '_mountains_lab_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MountainsLab::Application.config.session_store :active_record_store
